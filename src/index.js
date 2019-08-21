b8c4504b3cb27c043437959b59c1d13da40a6230
Danielle Martain <dmartain@gmail.com>
2019-08-21 15:03:26 -0500

    created saga and reducer for getting pets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Axios from 'axios';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+function* rootSaga() {
+    yield takeEvery('FETCH_PETS', fetchPets);
+}
+
+function* fetchPets(action) {
+    console.log('in fetchPets');
+    try {
+        const response = yield Axios.get('/api/pets')
+        const action = { type: 'SET_PETS', payload: response.data }
+        yield put(action);
+    }
+    catch (error) {
+        alert(`Couldn't get the pets. Try again later.`);
+        console.log('Error on GET', error);
+    }
+}
+
+const petListReducer = (state = [], action) => {
+    console.log('in petListReducer', action.payload);
+    switch (action.type) {
+        case 'SET_PETS':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+    combineReducers({
+        petListReducer,
+    }),
+    applyMiddleware(sagaMiddleware, logger),
+);
+
+sagaMiddleware.run(rootSaga);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
+
+
